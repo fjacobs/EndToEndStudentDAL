9fc17c7d6a3ecb4260e991ae44c0c64e58b77f0a
fer <foo@hotmail.com>
2018-06-14 13:21:50 +0200

    Create CRUD test

--- src/test/java/com/endtoend/StudentdalApplicationTests.java
+++ src/test/java/com/endtoend/StudentdalApplicationTests.java
@@ -8,20 +8,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Iterator;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentdalApplicationTests {
 
+    private static final String testStudentName = "TestJohn";
+    private static final String testTraining =  "TestTraining";
+
     @Autowired
     private StudentRepository studentRepository;
 
+
     @Test
+    public void testCrud() {
+        testCreateStudent();
+        testReadStudent();
+        testUpdateStudent();
+        testDeleteStudent();
+    }
+
+    private Student findTestStudent() {
+        Iterator<Student> all = studentRepository.findAll().iterator();
+        while (all.hasNext()) {
+            Student student = all.next();
+            if(student.getName().equals(testStudentName)) {
+                return student;
+            }
+        }
+        throw new RuntimeException("Can't find test student");
+    }
+
     public void testCreateStudent(){
         Student student = new Student();
-        student.setName("John");
-        student.setCourse("Micro Services Training");
+        student.setName(testStudentName);
+        student.setCourse(testTraining);
         student.setFee(30d);
         studentRepository.save(student);
+    }
+
+    public void testReadStudent(){
+        System.out.println(findTestStudent().toString());
+    }
+
+    public void testUpdateStudent(){
+        Student student = findTestStudent();
+        student.setFee(40d);
+        studentRepository.save(student);
+    }
+
+    public void testDeleteStudent(){
+        Student student = findTestStudent();
+        studentRepository.delete(student);
     }
 
 }
